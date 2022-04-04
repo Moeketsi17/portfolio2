3f4398b1a623b8b75807e66a4e0840f5709c7ee7
moeketsi <raymondkxng17@gmail.com>
2022-04-04 15:16:09 +0200

    commit

--- portfolio_2/app.js
+++ portfolio_2/app.js
@@ -1,6 +1,27 @@
-const burger = document.querySelectorAll(".burger");
-const navLinks = document.querySelector(".nav-links");
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li')
+const menu = document.querySelectorAll('.nav-links a')
 
 
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    burger.classList.toggle('open');
+
+    // links.forEach((link, index) => {
+        
+
+    //     if(link.style.animation){
+    //         link.style.animation = ''
+    //     } else {
+    //         link.style.animation = `navLinkFade 0.6s ease forwards ${index / 4 + 0.3}s `
+    //     }
+    //     console.log(index)
+        
+    // })
+
+    
+});
 
 
