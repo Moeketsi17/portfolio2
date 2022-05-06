c6c45929d47f4a0cb6c3f150029cb3f5641f7273
moeketsi <raymondkxng17@gmail.com>
2022-05-06 15:20:34 +0200

    commit

--- portfolio_2/app.js
+++ portfolio_2/app.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li')
-const menu = document.querySelectorAll('.nav-links a')
+const menu = document.querySelectorAll('.nav-links ul li a')
 
 
 
@@ -9,9 +9,20 @@ burger.addEventListener('click', () => {
     nav.classList.toggle('active');
     burger.classList.toggle('open');
 
+
     let tl = gsap.timeline()
-    tl.from('.nav-links ul li a', {y: -50 , opacity: 0, duration: 0.4, stagger: 0.25, delay: 0.2});
-    tl.from('.social-links-nav a', {y: 30, opacity:0, duration: 0.4}, '-=0.4')
+    tl.from('.nav-links ul li a', {
+        y: -50,
+        opacity: 0,
+        duration: 0.4,
+        stagger: 0.25,
+        delay: 0.2
+    });
+    tl.from('.social-links-nav a', {
+        y: 30,
+        opacity: 0,
+        duration: 0.4
+    }, '-=0.4')
 });
 
 
@@ -22,9 +33,22 @@ let tl2 = gsap.timeline({
     }
 })
 
-tl2.from('.hi-container h2', {y: -50, opacity: 0, duration:0.4});
-tl2.from('.name-container h1', {y: -50, opacity: 0, duration:0.4, stagger: 0.4});
-tl2.from('.resume a', {y: -50, opacity: 0, duration:0.4});
+tl2.from('.hi-container h2', {
+    y: -50,
+    opacity: 0,
+    duration: 0.4
+});
+tl2.from('.name-container h1', {
+    y: -50,
+    opacity: 0,
+    duration: 0.4,
+    stagger: 0.4
+});
+tl2.from('.resume a', {
+    y: -50,
+    opacity: 0,
+    duration: 0.4
+});
 
 
 
@@ -37,10 +61,10 @@ let projectElements = Array.from(document.querySelectorAll('.orject-card'));
 
 window.addEventListener('scroll', throttle(scanElements, 20))
 
-function scanElements(){
+function scanElements() {
     // get position of each div
     projectElements.forEach(element => {
-        if(isVisable(element)){
+        if (isVisable(element)) {
             element.classList.add('view');
         } else {
             element.classList.remove('view');
@@ -48,27 +72,21 @@ function scanElements(){
     })
 }
 
-function isVisable(element){
+function isVisable(element) {
     const elementDiv = element.getBoundingClientRect();
     let distanceFromTop = -200;
     return elementDiv.top - window.innerHeight < distanceFromTop ? true : false;
 }
 
-function throttle(fn, delay){
+function throttle(fn, delay) {
     let lastCall = 0;
     return (...arg) => {
         let context = this;
         let current = new Date().getTime();
-        if(current - lastCall < delay){
+        if (current - lastCall < delay) {
             return
         }
         lastCall = current;
         return fn.apply(context, ...arg)
     }
 }
-
-
-
-
-
-
