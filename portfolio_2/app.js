f2c58f4491227db6e3b6ff6209eda5d565fdb224
moeketsi <raymondkxng17@gmail.com>
2022-06-10 14:15:02 +0200

    commit

--- portfolio_2/app.js
+++ portfolio_2/app.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li')
-const menu = document.querySelectorAll('.nav-links ul li a')
+const links = document.querySelectorAll('.nav-links li a')
+// const menu = document.querySelectorAll('.nav-links ul li a')
 
 
 
@@ -9,20 +9,31 @@ burger.addEventListener('click', () => {
     nav.classList.toggle('active');
     burger.classList.toggle('open');
 
+    links.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.3}s `
+        }
+        console.log(index)
+        
+    })
 
-    let tl = gsap.timeline()
-    tl.from('.nav-links ul li a', {
-        y: -50,
-        opacity: 0,
-        duration: 0.4,
-        stagger: 0.25,
-        delay: 0.2
-    });
-    tl.from('.social-links-nav a', {
-        y: 30,
-        opacity: 0,
-        duration: 0.4
-    }, '-=0.4')
+    // let tl = gsap.timeline()
+    // tl.from('.nav-links ul li a', {
+    //     y: -50,
+    //     opacity: 0,
+    //     duration: 0.4,
+    //     stagger: 0.25,
+    //     delay: 0.2
+    // });
+    // tl.from('.social-links-nav a', {
+    //     y: 30,
+    //     opacity: 0,
+    //     duration: 0.4
+    // }, '-=0.4')
+
+
 });
 
 
